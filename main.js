6cf4ca83ffbd4beea8eeaf61e9addcaeb61c3892
Caeron Seabourne <caeking93@gmail.com>
2022-08-02 11:57:35 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let _headerScroll = {
+    headerEl: document.querySelector('header'),
+    scrollYPos: window.scrollY,
+    scrollCount: 0,
+
+    handleScroll() {
+        const _this = _headerScroll;
+        if (_this.scrollCount > 0) {
+            const headerEl = document.querySelector('header');
+            if (window.scrollY >= 300 && _this.scrollYPos < window.scrollY) {
+                headerEl.classList.add('off-screen');
+            } else if (_this.scrollYPos > window.scrollY) {
+                headerEl.classList.remove('off-screen');
+            }
+            _this.scrollYPos = window.scrollY;
+        }
+        _this.scrollCount++;
+    }
+};
+
+window.addEventListener('scroll', _headerScroll.handleScroll);
