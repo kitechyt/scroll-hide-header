5222e997cdbf4728e48acc501afe465d6919d316
kitechyt <kitechyt@gmail.com>
2022-08-10 16:38:44 +0100

    Added dummy content

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let _headerScroll = {
         const _this = _headerScroll;
         if (_this.scrollCount > 0) {
             const headerEl = document.querySelector('header');
-            if (window.scrollY >= 300 && _this.scrollYPos < window.scrollY) {
+            if (window.scrollY >= 50 && _this.scrollYPos < window.scrollY) {
                 headerEl.classList.add('off-screen');
             } else if (_this.scrollYPos > window.scrollY) {
                 headerEl.classList.remove('off-screen');
